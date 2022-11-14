f408edaf6569bd2f3ad28a162aa32bc8322eba0d
Houdayfa <91084419+Aschemlal@users.noreply.github.com>
2022-11-14 16:13:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+let player = document.getElementById("player")
+let v=(Math.random()>0.5)
+let td = []
+let text = []
+let winner=document.getElementById("winner");
+player.textContent= ((!v)? "X": "O")
+
+
+for(let i=1; i<10; i++){
+    td[i]=document.getElementById("t"+i)
+    
+}
+
+
+
+let win=()=>{
+    
+    if((td[1].textContent==td[5].textContent)&&(td[5].textContent==td[9].textContent)){
+        winner.textContent=td[1].textContent
+        
+    }
+    if((td[3].textContent==td[5].textContent)&&(td[5].textContent==td[7].textContent)){
+        winner.textContent=td[3].textContent
+        
+    }
+    if((td[1].textContent==td[2].textContent)&&(td[2].textContent==td[3].textContent)){
+        winner.textContent=td[1].textContent
+        
+    }
+    if((td[4].textContent==td[5].textContent)&&(td[5].textContent==td[6].textContent)){
+        winner.textContent=td[4].textContent
+        
+    }
+    if((td[7].textContent==td[8].textContent)&&(td[8].textContent==td[9].textContent)){
+        winner.textContent=td[7].textContent
+        
+    }
+    if((td[1].textContent==td[4].textContent)&&(td[4].textContent==td[7].textContent)){
+        winner.textContent=td[1].textContent
+        
+    }
+    if((td[2].textContent==td[5].textContent)&&(td[5].textContent==td[8].textContent)){
+        winner.textContent=td[2].textContent
+        
+    }
+    if((td[7].textContent==td[8].textContent)&&(td[8].textContent==td[9].textContent)){
+        winner.textContent=td[7].textContent
+        
+    }
+}
+let test;
+let jeu=a=> {
+
+    
+    if(!((td[a].textContent=="X")||(td[a].textContent=="O"))){
+        if(!v) {
+            td[a].textContent="X";
+            player.textContent="O";
+            
+        }
+            
+        if(v){
+            td[a].textContent="O";
+            player.textContent="X";
+            
+        }
+        v=!v
+        win()
+    }
+
+
+    
+        
+}
+
+
+
+
